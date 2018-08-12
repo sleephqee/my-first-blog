2a9f2710a73bf672b10fdbe36f9862a20e28c37b
sleephqee <sleephqee@gmail.com>
2018-08-12 16:54:06 +0700

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v4a#z%2#rx5jum#-thu$-=1is%btlxe!rslqv=&zotsvrs!2*r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '192.168.0.8']
 
 
 # Application definition
